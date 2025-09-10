de061ffa05ce0600b59c33218b321cb8b33b08fb
Ashiquejo <joashique@gmail.com>
2025-09-10 12:21:10 +0530

    Update Q5_infixToPostfix.c

--- Q5_infixToPostfix.c
+++ Q5_infixToPostfix.c
@@ -1,76 +1,105 @@
 #include <stdio.h>
-#include <ctype.h>
 #define MAX 100
+#include <ctype.h>
+#include <math.h>
 
 char stack[MAX];
 int top = -1;
 
-void push(char c) {
-    if (top < MAX - 1) {
-        stack[++top] = c;
-    }
+void push(char c) {stack[++top] = c;}
+
+char pop(){
+    if (top == -1){return '\0';} 
+    else {return stack[top--];}
 }
 
-char pop() {
-    return (top == -1) ? '\0' : stack[top--];
-}
-
-char peek() {
-    return (top == -1) ? '\0' : stack[top];
+char peek(){
+    if (top == -1) {return '\0';} 
+    else{return stack[top];}
 }
 
 int precedence(char c) {
-    if (c == '^') return 3;
-    if (c == '*' || c == '/') return 2;
-    if (c == '+' || c == '-') return 1;
-    return 0;
+    if (c == '^') {return 3;} 
+    else if (c == '*' || c == '/') {return 2;}
+    else if (c == '+' || c == '-'){return 1;} 
+    else{return 0;}
 }
 
 int isOperator(char c) {
-    return (c == '^' || c == '*' || c == '/' || c == '+' || c == '-');
+    if (c == '^' || c == '*' || c == '/' || c == '+' || c == '-') {return 1;} 
+    else{return 0;}
 }
 
-void infixToPostfix(char infix[], char postfix[]) {
+void infinixtopostfix(char infix[], char postfix[]) {
     int i = 0, j = 0;
     char c;
 
     while ((c = infix[i]) != '\0') {
-        if (isalnum(c)) { // Operand (letter or number)
+       
+        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
             postfix[j++] = c;
         }
-        else if (c == '(') {
-            push(c);
+        else if(isdigit(c)){
+            postfix[j++] = c;
         }
+        else if (c == '(')push(c);
         else if (c == ')') {
             while (top != -1 && peek() != '(') {
                 postfix[j++] = pop();
             }
-            if (peek() == '(') pop();
+            if (peek() == '(') {
+                pop();  
+            }
         }
         else if (isOperator(c)) {
-            while (top != -1 && isOperator(peek()) &&
-                   precedence(peek()) >= precedence(c)) {
+            while (top != -1 && isOperator(peek()) && precedence(peek()) >= precedence(c)) {
                 postfix[j++] = pop();
             }
             push(c);
         }
         i++;
     }
-
     while (top != -1) {
         postfix[j++] = pop();
     }
 
-    postfix[j] = '\0';
+    postfix[j] = '\0';  
+}
+
+int evaluatePostfix(char postfix[]) {
+    int i = 0,a,b,r;
+    char c;
+
+    top = -1;
+
+    while ((c = postfix[i]) != '\0') {
+        if (isdigit(c)) {
+            push(c - '0');
+        } else if (isOperator(c)) {
+            b = pop();
+            a = pop();
+            switch (c) {
+                case '+': r = a + b; break;
+                case '-': r = a -b; break;
+                case '*': r = a *b; break;
+                case '/': r = a / b; break;
+                case '^': r = (int)pow(a,b); break;
+                default: r = 0; break;
+            }
+            push(r);
+        }
+        i++;
+    }
+    return pop();
 }
 
 int main() {
     char infix[MAX], postfix[MAX];
     printf("Enter the infix expression: ");
     scanf("%s", infix);
-
-    infixToPostfix(infix, postfix);
+    infinixtopostfix(infix, postfix);
     printf("The postfix expression is: %s\n", postfix);
-
+    int result = evaluatePostfix(postfix);
+    printf("The result of the postfix expression is: %d\n", result);
     return 0;
 }
